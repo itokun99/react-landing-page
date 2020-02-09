b3ce97c2934b8f95c332d61cbea8324f6650fcd2
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-02-09 22:35:04 +0700

    update landing page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,4 +3,21 @@ import ImgCoding from './img-coding.svg';
 import ImgLearning from './img-learning.svg';
 import ImgProject from './img-project.svg';
 
-export { ImgAboutIllustration, ImgCoding, ImgLearning, ImgProject };
+// Project
+import ImgProjectBkp from './projects/img-project-bkp.png';
+import ImgProjectLptq from './projects/img-project-lptq.png';
+import ImgProjectPtsp from './projects/img-project-ptsp.png';
+import ImgProjectAkun from './projects/img-project-akun.png';
+import ImgProjectKasihinaja from './projects/img-project-kasihaja.png';
+
+export {
+  ImgAboutIllustration,
+  ImgCoding,
+  ImgLearning,
+  ImgProject,
+  ImgProjectBkp,
+  ImgProjectLptq,
+  ImgProjectPtsp,
+  ImgProjectAkun,
+  ImgProjectKasihinaja
+};
--- src/components/organisms/ProjectSection.js
+++ src/components/organisms/ProjectSection.js
@@ -1,6 +1,47 @@
 import { React, useMemo } from 'libraries';
 import { Container, Section, Image } from 'components/atoms';
-import { ImgCoding, ImgLearning } from 'assets';
+import {
+  ImgCoding,
+  ImgLearning,
+  ImgProjectBkp,
+  ImgProjectLptq,
+  ImgProjectAkun,
+  ImgProjectPtsp,
+  ImgProjectKasihinaja
+} from 'assets';
+
+const projects = [
+  {
+    id: 1,
+    title: 'BKP Apps',
+    desc: 'Aplikasi report karantina',
+    image: ImgProjectBkp
+  },
+  {
+    id: 2,
+    title: 'LPTQ Papua',
+    desc: 'Website LPTQ Kementrian agama provinsi Papua',
+    image: ImgProjectLptq
+  },
+  {
+    id: 3,
+    title: 'PTSP',
+    desc: 'Pelayanan Terpadu Satu Pintu kemenag provinsi Papua',
+    image: ImgProjectPtsp
+  },
+  {
+    id: 4,
+    title: 'AKUN',
+    desc: 'Aplikasi user auth management',
+    image: ImgProjectAkun
+  },
+  {
+    id: 5,
+    title: 'Kasihin Aja',
+    desc: 'Website online pencarian dan sedekah barang bekas',
+    image: ImgProjectKasihinaja
+  }
+];
 
 const ProjectSection = () =>
   useMemo(
@@ -13,9 +54,20 @@ const ProjectSection = () =>
             </div>
             <div className="ProjectSection__content">
               <div className="ProjectSection__row">
-                {[1, 2, 3, 4, 5, 6].map(val => (
-                  <div key={val} className="ProjectSection__column">
-                    <div className="ProjectSection__project-item"></div>
+                {projects.map(project => (
+                  <div key={project.id} className="ProjectSection__column">
+                    <div className="ProjectSection__project-item">
+                      <Image
+                        backgroundImage
+                        className="ProjectSection__project-image"
+                        source={project.image}
+                      />
+                      <div className="ProjectSection__project-body">
+                        <h4 className="ProjectSection__project-title">
+                          {project.title}
+                        </h4>
+                      </div>
+                    </div>
                   </div>
                 ))}
               </div>
