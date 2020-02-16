505f1244dc2c92c0d961fcf1abba7a03bbae9893
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-02-16 13:16:08 +0700

    add responsive design

--- src/components/molecules/Nav.js
+++ src/components/molecules/Nav.js
@@ -1,30 +1,55 @@
-import { React, NavLink, PropTypes, useMemo } from 'libraries';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { React, NavLink, PropTypes, useMemo, useState } from 'libraries';
 
-const Nav = ({ navigation }) =>
-  useMemo(
+const Nav = ({ navigation }) => {
+  const [isActive, showMenu] = useState(false);
+
+  return useMemo(
     () => (
-      <nav className="Nav">
-        {navigation &&
-          navigation.map((nav, index) => (
-            <React.Fragment>
-              {/* <NavLink
-                className="Nav__item"
-                to={nav.url}
-                key={index}
-                title={nav.title}
-                activeClassName="Nav__item--active"
-              >
-                {nav.title}
-              </NavLink> */}
-              <a key={index} className="Nav__item" href={nav.url}>
-                {nav.title}
-              </a>
-            </React.Fragment>
-          ))}
-      </nav>
+      <React.Fragment>
+        <div onClick={() => showMenu(true)} className="Nav__burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav className={`Nav ${isActive ? 'Nav--active' : ''}`}>
+          <div
+            onClick={() => showMenu(false)}
+            className="Nav__burger Nav__burger--close"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {navigation &&
+            navigation.map((nav, index) => (
+              <React.Fragment>
+                {/* <NavLink
+                  className="Nav__item"
+                  to={nav.url}
+                  key={index}
+                  title={nav.title}
+                  activeClassName="Nav__item--active"
+                >
+                  {nav.title}
+                </NavLink> */}
+                <a
+                  key={index}
+                  onClick={() => showMenu(false)}
+                  className="Nav__item"
+                  href={nav.url}
+                >
+                  {nav.title}
+                </a>
+              </React.Fragment>
+            ))}
+        </nav>
+      </React.Fragment>
     ),
-    [navigation]
+    [isActive, navigation]
   );
+};
 
 Nav.propTypes = {
   navigation: PropTypes.array
