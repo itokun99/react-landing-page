337ef65a1f43386b78d89ca8d758089fad6745e8
Indrawan Lisanto <indrawan@aleph-labs.com>
2020-02-16 16:01:05 +0700

    release version 1.0.0

---
+++ build/precache-manifest.9deb44c716c6afa42e39a1b84d2bc9ec.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a69df9e4da15bdae531606b41206557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edaafcc406a76605a144",
+    "url": "/static/css/main.89a4e698.chunk.css"
+  },
+  {
+    "revision": "46781bf3282f8fefa288",
+    "url": "/static/js/2.68bb6393.chunk.js"
+  },
+  {
+    "revision": "edaafcc406a76605a144",
+    "url": "/static/js/main.422b9e84.chunk.js"
+  },
+  {
+    "revision": "8c50d640276571c9529c",
+    "url": "/static/js/runtime-main.cb6b3845.js"
+  },
+  {
+    "revision": "5806763981eb2ca2f929d346f78013bd",
+    "url": "/static/media/img-about-illustration.58067639.svg"
+  },
+  {
+    "revision": "c22746bf6c996facd85bb0efae6f39f6",
+    "url": "/static/media/img-coding.c22746bf.svg"
+  },
+  {
+    "revision": "56a0f1a94704f46a00a5ad1247cb34c0",
+    "url": "/static/media/img-learning.56a0f1a9.svg"
+  },
+  {
+    "revision": "dab774920cf547ea1d06500d0d59bd99",
+    "url": "/static/media/img-logo.dab77492.svg"
+  },
+  {
+    "revision": "b6c610d040f3261291d3d60fd32ee541",
+    "url": "/static/media/img-project-akun.b6c610d0.png"
+  },
+  {
+    "revision": "6cc4e367f06322c978364a90efe19ac8",
+    "url": "/static/media/img-project-bkp.6cc4e367.png"
+  },
+  {
+    "revision": "cd80c24cbbc7ee6d6f8ef112d8f76d45",
+    "url": "/static/media/img-project-kasihaja.cd80c24c.png"
+  },
+  {
+    "revision": "8b8e3138827cc1f7c92eff6cb125fb3d",
+    "url": "/static/media/img-project-lptq.8b8e3138.png"
+  },
+  {
+    "revision": "c67992daa39e596c4d27d35ca8f3b94b",
+    "url": "/static/media/img-project-ptsp.c67992da.png"
+  },
+  {
+    "revision": "3a9c22f63363fe41b9edac637cd9b11b",
+    "url": "/static/media/img-project.3a9c22f6.svg"
+  }
+]);
